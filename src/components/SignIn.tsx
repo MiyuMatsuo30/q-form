b98fd4914670eb6ceeb63f315eb26dd34a02141e
MiyuMatsuo30 <matsuo30m@gmail.com>
2024-09-21 00:45:16 +0900

    ログイン画面形だけ

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,28 @@
+import { AuthProvider, AppProvider, SignInPage } from '@toolpad/core';
+import { useTheme } from '@mui/material/styles';
+
+const providers = [{ id: 'credentials', name: 'Email and Password' }];
+
+const signIn: (provider: AuthProvider, formData: FormData) => void = async(
+    provider,
+    formData,
+) => {
+    const promise = new Promise<void>((resolve) => {
+        setTimeout(() => {
+            alert (
+                `Signing in with "${provider.name}" and credentials: ${formData.get('email')}, ${formData.get('password')}`
+            );
+            resolve();
+        }, 300);
+    });
+    return promise;
+};
+
+export default function CredentialSignInPage() {
+    const theme = useTheme();
+    return (
+        <AppProvider theme={theme} className="non-flex">
+            <SignInPage signIn={signIn} providers={providers} />
+        </AppProvider>
+    );
+}
